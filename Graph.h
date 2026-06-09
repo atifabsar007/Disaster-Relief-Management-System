e23317852eb01fcf40f738053177c309b1ae7675
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 13:36:34 +0600

    Add Graph class with basic graph functionalities

--- Graph.h
+++ Graph.h
@@ -1,1 +1,25 @@
+// Graph.h
 
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph {
+public:
+    int V;
+    vector<pair<int,int>> adj[100];
+
+    Graph(int v);
+
+    void addEdge(int u, int v, int w);
+
+    void BFS(int start);
+    void DFSUtil(int v, vector<bool> &visited);
+    void DFS(int start);
+
+    void dijkstra(int src, vector<int> &dist);
+};
+
+#endif
