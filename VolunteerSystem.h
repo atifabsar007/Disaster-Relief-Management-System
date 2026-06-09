71ce1bc1b4df77b3813f185936d50b904419e9bb
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:21:08 +0600

    Update VolunteerSystem.h

--- VolunteerSystem.h
+++ VolunteerSystem.h
@@ -1,30 +1,27 @@
-#ifndef VOLUNTEER_H
-#define VOLUNTEER_H
+#ifndef VOL_H
+#define VOL_H
 
 #include <bits/stdc++.h>
 using namespace std;
 
 struct Volunteer {
-    string name;
-    string skill;
+    string name, skill;
 };
 
 class VolunteerSystem {
 public:
     vector<Volunteer> v;
 
-    void add(string n, string s) {
-        v.push_back({n, s});
+    void add(string n,string s){
+        v.push_back({n,s});
     }
 
-    int size() {
-        return v.size();
-    }
+    int size(){ return v.size(); }
 
-    void show() {
-        cout << "\n🤝 VOLUNTEERS\n";
-        for (auto &x : v)
-            cout << x.name << " | " << x.skill << endl;
+    void show(){
+        cout<<"\n🤝 VOLUNTEERS\n";
+        for(auto &x:v)
+            cout<<x.name<<" | "<<x.skill<<endl;
     }
 };
 
