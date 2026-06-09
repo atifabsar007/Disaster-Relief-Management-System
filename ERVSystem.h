5027a23e1dc8858610fb5dc5a6600e90a19a69c1
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:21:20 +0600

    Update ERVSystem.h

--- ERVSystem.h
+++ ERVSystem.h
@@ -7,42 +7,38 @@ using namespace std;
 class ERVSystem {
 public:
     struct ERV {
-        string id;
-        string location;
-        bool busy = false;
+        string id, loc;
+        bool busy=false;
     };
 
     vector<ERV> v;
 
-    void add(string id, string loc) {
-        v.push_back({id, loc, false});
+    void add(string id,string l){
+        v.push_back({id,l,false});
     }
 
-    void dispatch(string loc) {
-        for (auto &x : v) {
-            if (!x.busy) {
-                x.busy = true;
-                cout << "\n🚑 ERV DISPATCHED\n";
-                cout << "ID: " << x.id << endl;
-                cout << "From: " << x.location << endl;
-                cout << "To: " << loc << endl;
+    void dispatch(string loc){
+        for(auto &x:v){
+            if(!x.busy){
+                x.busy=true;
+                cout<<"\n🚑 ERV DISPATCHED\n";
+                cout<<"ID: "<<x.id<<"\nTo: "<<loc<<endl;
                 return;
             }
         }
-        cout << "No ERVs available\n";
+        cout<<"No ERVs Available\n";
     }
 
-    int busyCount() {
-        int c = 0;
-        for (auto &x : v)
-            if (x.busy) c++;
+    int busyCount(){
+        int c=0;
+        for(auto &x:v) if(x.busy) c++;
         return c;
     }
 
-    void show() {
-        cout << "\n🚑 ERVs\n";
-        for (auto &x : v)
-            cout << x.id << " | " << (x.busy ? "BUSY" : "FREE") << endl;
+    void show(){
+        cout<<"\n🚑 ERVs\n";
+        for(auto &x:v)
+            cout<<x.id<<" | "<<(x.busy?"BUSY":"FREE")<<endl;
     }
 };
 
