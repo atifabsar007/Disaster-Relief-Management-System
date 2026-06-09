9c84f3c18da47380aadb51dae7fd025303fd96bb
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 13:48:38 +0600

    Update Emergency.h

--- Emergency.h
+++ Emergency.h
@@ -3,6 +3,9 @@
 #ifndef EMERGENCY_H
 #define EMERGENCY_H
 
+#include <bits/stdc++.h>
+using namespace std;
+
 struct Emergency {
     int id;
     int city;
@@ -10,6 +13,12 @@ struct Emergency {
 
     bool operator<(const Emergency &e) const {
         return severity < e.severity; // max heap
+    }
+
+    void print(string cityName[]) {
+        cout << "ID: " << id
+             << " | City: " << cityName[city]
+             << " | Severity: " << severity << endl;
     }
 };
 
