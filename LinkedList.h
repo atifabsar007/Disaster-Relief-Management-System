4642cc38880df539a586fdd4866a21cb4d72db29
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 13:37:58 +0600

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,38 @@
+// LinkedList.h
 
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    string data;
+    Node* next;
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = NULL;
+    }
+
+    void insert(string s) {
+        Node* n = new Node();
+        n->data = s;
+        n->next = head;
+        head = n;
+    }
+
+    void display() {
+        Node* temp = head;
+        while(temp) {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+    }
+};
+
+#endif
