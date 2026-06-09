6d8c451c0972f756360dc03d5b1651e2098b9b9e
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:56:28 +0600

    Update Dashboard.h

--- Dashboard.h
+++ Dashboard.h
@@ -7,13 +7,13 @@ using namespace std;
 class Dashboard {
 public:
     void show(int v,int c,int sh,int erv,int vol,int f,int w){
-        cout<<"\n🚨 DASHBOARD\n";
-        cout<<"Victims: "<<v<<endl;
-        cout<<"Critical: "<<c<<endl;
-        cout<<"Shelters: "<<sh<<endl;
-        cout<<"ERV Busy: "<<erv<<endl;
-        cout<<"Volunteers: "<<vol<<endl;
-        cout<<"Food: "<<f<<" Water: "<<w<<endl;
+        cout << "\n🚨 DASHBOARD\n";
+        cout << "Victims: " << v << endl;
+        cout << "Critical: " << c << endl;
+        cout << "Shelters: " << sh << endl;
+        cout << "ERV Busy: " << erv << endl;
+        cout << "Volunteers: " << vol << endl;
+        cout << "Food: " << f << " Water: " << w << endl;
     }
 };
 
