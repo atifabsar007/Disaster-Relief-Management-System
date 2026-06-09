a253b2bdde11c3212a312a2ab3f8f4b8b95268d7
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:20:51 +0600

    Update SupplySystem.h

--- SupplySystem.h
+++ SupplySystem.h
@@ -6,27 +6,21 @@ using namespace std;
 
 class SupplySystem {
 public:
-    int food = 100;
-    int water = 100;
-    int medicine = 50;
-    int blanket = 80;
+    int food=100, water=100, med=50, blanket=80;
 
-    void use(string t) {
-        if (t == "food") food--;
-        if (t == "water") water--;
-        if (t == "medicine") medicine--;
-        if (t == "blanket") blanket--;
+    void use(string t){
+        if(t=="food") food--;
+        if(t=="water") water--;
+        if(t=="med") med--;
+        if(t=="blanket") blanket--;
 
-        if (food < 20 || water < 20)
-            cout << "⚠ LOW SUPPLY ALERT!\n";
+        cout<<"✅ Supply Updated\n";
     }
 
-    void show() {
-        cout << "\n📦 SUPPLIES\n";
-        cout << "Food: " << food
-             << " Water: " << water
-             << " Medicine: " << medicine
-             << " Blanket: " << blanket << endl;
+    void show(){
+        cout<<"\n📦 SUPPLIES\n";
+        cout<<"Food:"<<food<<" Water:"<<water
+            <<" Med:"<<med<<" Blanket:"<<blanket<<endl;
     }
 };
 
