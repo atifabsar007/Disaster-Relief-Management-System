8f8807034eeef9c5b3426cd90c135198965e1b80
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 13:35:33 +0600

    Update Emergency.h

--- Emergency.h
+++ Emergency.h
@@ -1,1 +1,16 @@
+// Emergency.h
 
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
+
+struct Emergency {
+    int id;
+    int city;
+    int severity;
+
+    bool operator<(const Emergency &e) const {
+        return severity < e.severity; // max heap
+    }
+};
+
+#endif
