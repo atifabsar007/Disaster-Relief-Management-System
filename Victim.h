daf1b27f46743597940c7cb7cb2f6a7d9e06c9ec
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:55:04 +0600

    Update Victim.h

--- Victim.h
+++ Victim.h
@@ -15,7 +15,7 @@ public:
     Victim* head = NULL;
 
     void add(string n,int a,string g,string c,string s){
-        Victim* v=new Victim();
+        Victim* v = new Victim();
         v->name=n;
         v->age=a;
         v->gender=g;
@@ -26,30 +26,34 @@ public:
     }
 
     void show(){
-        cout<<"\n🏠 VICTIMS LIST\n";
-        for(Victim* t=head;t;t=t->next){
-            cout<<t->name<<" | "<<t->age
-                <<" | "<<t->gender
-                <<" | "<<t->condition
-                <<" | "<<t->shelter<<endl;
+        cout << "\n🏠 VICTIMS LIST\n";
+        for(auto t=head;t;t=t->next){
+            cout << t->name << " | " << t->age
+                 << " | " << t->gender
+                 << " | " << t->condition
+                 << " | " << t->shelter << endl;
         }
     }
 
     void search(string name){
-        for(Victim* t=head;t;t=t->next){
+        for(auto t=head;t;t=t->next){
             if(t->name==name){
-                cout<<"\n🔍 FOUND VICTIM\n";
-                cout<<"Name: "<<t->name<<endl;
-                cout<<"Age: "<<t->age<<endl;
-                cout<<"Condition: "<<t->condition<<endl;
-                cout<<"Shelter: "<<t->shelter<<endl;
+                cout << "\n🔍 FOUND VICTIM\n";
+                cout << "Name: " << t->name << endl;
+                cout << "Age: " << t->age << endl;
+                cout << "Condition: " << t->condition << endl;
+                cout << "Shelter: " << t->shelter << endl;
                 return;
             }
         }
-        cout<<"❌ Not Found\n";
+        cout << "❌ Not Found\n";
     }
 
-    int count(){ int c=0; for(auto t=head;t;t=t->next) c++; return c; }
+    int count(){
+        int c=0;
+        for(auto t=head;t;t=t->next) c++;
+        return c;
+    }
 
     int critical(){
         int c=0;
