9df85a459f4731bb312b0ad3a855d74b0d5bcb64
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:56:13 +0600

    Update ERVSystem.h

--- ERVSystem.h
+++ ERVSystem.h
@@ -17,47 +17,48 @@ public:
         string id, loc;
 
         cout << "\n🚑 ADD ERV\n";
-        cout << "Enter ERV ID: ";
+        cout << "ID: ";
         cin >> id;
 
         cout << "Base Location: ";
         cin >> loc;
 
-        v.push_back({id, loc, false});
+        v.push_back({id,loc,false});
 
-        cout << "✅ ERV Added Successfully\n";
+        cout << "✅ ERV Added\n";
     }
 
     void dispatch(string dest){
-        cout << "\n🚑 ERV DISPATCH SYSTEM\n";
+        cout << "\n🚑 ERV DISPATCH\n";
         cout << "Searching available ERVs...\n";
 
         for(auto &x:v){
             if(!x.busy){
-                x.busy = true;
+                x.busy=true;
 
                 cout << "\n✅ ERV ASSIGNED\n";
-                cout << "ID   : " << x.id << endl;
-                cout << "FROM : " << x.loc << endl;
-                cout << "TO   : " << dest << endl;
+                cout << "ID: " << x.id << endl;
+                cout << "FROM: " << x.loc << endl;
+                cout << "TO: " << dest << endl;
                 return;
             }
         }
 
-        cout << "❌ No ERVs Available\n";
+        cout << "❌ NO ERV AVAILABLE\n";
     }
 
     int busyCount(){
         int c=0;
-        for(auto &x:v) if(x.busy) c++;
+        for(auto &x:v)
+            if(x.busy) c++;
         return c;
     }
 
     void show(){
-        cout << "\n🚑 ERV STATUS\n";
+        cout << "\n🚑 ERVs\n";
         for(auto &x:v){
             cout << x.id << " | "
-                 << (x.busy ? "BUSY" : "FREE") << endl;
+                 << (x.busy?"BUSY":"FREE") << endl;
         }
     }
 };
