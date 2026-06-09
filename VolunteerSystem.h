80d210b094f318efe5c84bfb5fc1282edef14b0e
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:49:04 +0600

    Update VolunteerSystem.h

--- VolunteerSystem.h
+++ VolunteerSystem.h
@@ -5,24 +5,46 @@
 using namespace std;
 
 struct Volunteer {
-    string name, skill;
+    string name;
+    string skill;
 };
 
 class VolunteerSystem {
 public:
     vector<Volunteer> v;
 
-    void add(string n,string s){
-        v.push_back({n,s});
+    void add(){
+        string n;
+        int choice;
+
+        cout << "\n🤝 ENTER VOLUNTEER NAME: ";
+        cin >> n;
+
+        cout << "\nSELECT SKILL:\n";
+        cout << "1. Medical 🏥\n";
+        cout << "2. Rescue 🚑\n";
+        cout << "3. Logistics 📦\n";
+        cout << "Choice: ";
+        cin >> choice;
+
+        string skill;
+        if(choice==1) skill="Medical";
+        else if(choice==2) skill="Rescue";
+        else skill="Logistics";
+
+        v.push_back({n,skill});
+
+        cout << "✅ Volunteer Added\n";
+    }
+
+    void show(){
+        cout << "\n🤝 VOLUNTEERS\n";
+        for(auto &x:v){
+            cout << x.name << " | " << x.skill << endl;
+        }
     }
 
     int size(){ return v.size(); }
-
-    void show(){
-        cout<<"\n🤝 VOLUNTEERS\n";
-        for(auto &x:v)
-            cout<<x.name<<" | "<<x.skill<<endl;
-    }
 };
 
 #endif
