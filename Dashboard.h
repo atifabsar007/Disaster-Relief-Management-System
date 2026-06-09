99ffd21728601e656f7c29915062fa1ef05bd138
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:21:34 +0600

    Update Dashboard.h

--- Dashboard.h
+++ Dashboard.h
@@ -1,25 +1,19 @@
-#ifndef DASHBOARD_H
-#define DASHBOARD_H
+#ifndef DASH_H
+#define DASH_H
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Dashboard {
 public:
-    void show(int victims, int critical,
-              int shelters, int erv,
-              int volunteers, int food, int water) {
-
-        cout << "\n🚨 CONTROL DASHBOARD\n";
-        cout << "========================\n";
-        cout << "Victims      : " << victims << endl;
-        cout << "Critical     : " << critical << endl;
-        cout << "Shelters     : " << shelters << endl;
-        cout << "ERVs Busy    : " << erv << endl;
-        cout << "Volunteers   : " << volunteers << endl;
-        cout << "Food Stock   : " << food << endl;
-        cout << "Water Stock  : " << water << endl;
-        cout << "========================\n";
+    void show(int v,int c,int sh,int erv,int vol,int f,int w){
+        cout<<"\n🚨 DASHBOARD\n";
+        cout<<"Victims: "<<v<<endl;
+        cout<<"Critical: "<<c<<endl;
+        cout<<"Shelters: "<<sh<<endl;
+        cout<<"ERV Busy: "<<erv<<endl;
+        cout<<"Volunteers: "<<vol<<endl;
+        cout<<"Food: "<<f<<" Water: "<<w<<endl;
     }
 };
 
