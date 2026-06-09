8d0e832f3839bafc098dd1d9bf6b8a85958bcaed
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 15:07:10 +0600

    Update Dashboard.h

--- Dashboard.h
+++ Dashboard.h
@@ -6,33 +6,20 @@ using namespace std;
 
 class Dashboard {
 public:
-    int totalVictims = 0;
-    int criticalPatients = 0;
-    int activeShelters = 0;
-    int ervDeployed = 0;
+    void show(int victims, int critical,
+              int shelters, int erv,
+              int volunteers, int food, int water) {
 
-    int foodPackets = 100;
-    int waterBottles = 100;
-    int medicineKits = 50;
-    int blankets = 80;
-
-    void showDashboard() {
-
-        cout << "\n🚨 DISASTER CONTROL DASHBOARD\n";
-        cout << "=================================\n";
-
-        cout << "👥 Total Victims      : " << totalVictims << endl;
-        cout << "🏥 Critical Patients  : " << criticalPatients << endl;
-        cout << "🏠 Active Shelters    : " << activeShelters << endl;
-        cout << "🚑 ERVs Deployed      : " << ervDeployed << endl;
-
-        cout << "\n📦 RESOURCE STATUS\n";
-        cout << "Food Packets   : " << foodPackets << endl;
-        cout << "Water Bottles  : " << waterBottles << endl;
-        cout << "Medicine Kits  : " << medicineKits << endl;
-        cout << "Blankets       : " << blankets << endl;
-
-        cout << "=================================\n";
+        cout << "\n🚨 CONTROL DASHBOARD\n";
+        cout << "========================\n";
+        cout << "Victims      : " << victims << endl;
+        cout << "Critical     : " << critical << endl;
+        cout << "Shelters     : " << shelters << endl;
+        cout << "ERVs Busy    : " << erv << endl;
+        cout << "Volunteers   : " << volunteers << endl;
+        cout << "Food Stock   : " << food << endl;
+        cout << "Water Stock  : " << water << endl;
+        cout << "========================\n";
     }
 };
 
